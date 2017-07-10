b84ee51b80790506067d7530d3fca550b7d5b400
bboc <bernhard.bockelbrink@gmail.com>
2017-07-10 10:48:58 +0200

    plugin to update colors

---
+++ ogplugins/update_colors_and_fonts.py
@@ -0,0 +1,28 @@
+"""
+A plugin to update fonts and adapt font size.
+"""
+
+from functools import partial
+from omnigraffle.data_model import Document, TextContainer
+
+def main(document, config, canvas=None, verbose=None):
+    """
+    Replace fonts as defined in config.yaml
+
+    fonts:
+      source-font: [replacement-font, size-delta (int)]
+    """
+
+    def replace(config, element):
+        if isinstance(element, TextContainer):
+            if element.item.text(): # element might still have no text
+                current_font = element.text.font()
+                if config['fonts'].has_key(current_font):
+                    new_font, size_delta = config['fonts'][current_font]
+                    size = element.text.size()
+                    new_size = size + size_delta
+                    element.text.size.set(new_size)
+                    element.text.font.set(new_font)
+
+    d = Document(document)
+    d.walk(partial(replace, config))
